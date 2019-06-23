8f08f20e4576f64ab2ed3f06b8462e4797597264
FengWenZhu <819902702@qq.com>
2019-06-23 18:48:50 +0800

    将IItemService重命名为ItemService

--- mall-manager-web/src/main/java/cn/com/controller/ItemController.java
+++ mall-manager-web/src/main/java/cn/com/controller/ItemController.java
@@ -1,10 +1,10 @@
 package cn.com.controller;
 
 import cn.com.pojo.Item;
-import cn.com.service.IItemService;
+import cn.com.service.ItemService;
+import com.alibaba.dubbo.config.annotation.Reference;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,8 +23,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/item")
 public class ItemController {
 
-    @Autowired
-    private IItemService itemService;
+    @Reference(version = "1.0.0")
+    private ItemService itemService;
 
     /**
      * Description：根据主键查询商品信息
