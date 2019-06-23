d25eb64ac62295b6a0222de3ddf2d87d3f7ed13c
FengWenZhu <819902702@qq.com>
2019-06-23 18:52:43 +0800

    添加dubbo支持，使用soa架构

--- mall-manager-web/src/main/java/cn/com/ManagerWebApplicationStarter.java
+++ mall-manager-web/src/main/java/cn/com/ManagerWebApplicationStarter.java
@@ -2,10 +2,16 @@ package cn.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
+@ComponentScan("cn.com.*")
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class ManagerWebApplicationStarter {
 
     public static void main(String[] args) {
--- mall-manager/mall-manager-service/src/main/java/cn/com/ManagerServiceApplicationStarter.java
+++ mall-manager/mall-manager-service/src/main/java/cn/com/ManagerServiceApplicationStarter.java
@@ -1,13 +1,19 @@
 package cn.com;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableDubbo()
 @MapperScan("cn.com.mapper")
 @ComponentScan("cn.com.*")
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class ManagerServiceApplicationStarter {
 
     public static void main(String[] args) throws InterruptedException {
